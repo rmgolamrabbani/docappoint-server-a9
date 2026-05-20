43b3500e476f2d4f3d03b217ddbcf0290fadd66b
G. <rmgolamrabbani@gmail.com>
2026-05-20 23:19:54 +0600

    same issue

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const verifyToken = async (req, res, next) => {
   }
 
   try {
-    const authUrl = "https://docappoint-client-a9.vercel.app";
+    const authUrl = "https://docappoint-client-a9.vercel.app/api/auth/get-session";
     
     const authRes = await fetch(authUrl, {
       headers: {
